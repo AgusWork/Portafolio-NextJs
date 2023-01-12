5ae38f14a2c3950982d829263aaef0cb63a7edd8
acordoba <aguscordoba11@hotmail.com>
2023-01-12 16:21:06 -0300

    pre carga del font para mejor fcp

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,11 +21,11 @@ class MyDocument extends Document {
 			<Html lang="es">
 				<Head>
 					{CssBaseline.flush()}
+					<link rel="preload" as="font"></link>
 					<link
 						rel="stylesheet"
 						href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
 					/>
-
 					<link rel="shortcut icon" href="/portafolio.png" type="image/x-icon" />
 					<link rel="icon" href="/portafolio.png" type="image/x-icon" />
 
